b9849dd24f65b3c19a81168f477a1c5758759006
soliverson <stacy.oliverson@gmail.com>
2024-09-26 17:28:26 -0600

    Finished Assignment 2 Database

---
+++ database/assignment2.sql
@@ -0,0 +1,39 @@
+-- 1
+INSERT INTO account (
+        account_firstname,
+        account_lastname,
+        account_email,
+        account_password
+    )
+VALUES (
+        'Tony',
+        'Stark',
+        'tony@starkent.com',
+        'Iam1ronM@n'
+    );
+-- 2
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_email = 'tony@starkent.com';
+-- 3
+DELETE FROM account
+WHERE account_email = 'tony@starkent.com';
+-- 4
+UPDATE inventory
+SET inv_description = REPLACE(
+        inv_description,
+        'small interiors',
+        'a huge interior'
+    )
+WHERE inv_model = 'Hummer';
+--5
+SELECT i.inv_make,
+    i.inv_model,
+    c.classification_name
+FROM inventory i
+    INNER JOIN classification c ON i.classification_id = c.classification_id
+WHERE c.classification_name = 'Sport';
+-- 6
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
