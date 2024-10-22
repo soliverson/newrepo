0b890b74159428d61e9f85b4699280ce00570867
soliverson <stacy.oliverson@gmail.com>
2024-10-22 13:26:18 -0600

    minor error fixes

--- server.js
+++ server.js
@@ -14,22 +14,20 @@ const static = require("./routes/static");
 const baseController = require("./controllers/baseController");
 const inventoryRoute = require("./routes/inventoryRoute");
 const utilities = require("./utilities");
-const session = require("express-session")
+const session = require("express-session");
 const pool = require('./database/');
 const accountRoutes = require("./routes/accountRoute");
 const bodyParser = require("body-parser");
 const classificationRoute = require("./routes/classificationRoute");
 const cookieParser = require("cookie-parser");
 
-
 /* ***********************
  * View Engine and Templates
  *************************/
 
-
 /* ***********************
  * Middleware
- * ************************/
+ *************************/
 app.use(session({
   store: new (require('connect-pg-simple')(session))({
     createTableIfMissing: true,
@@ -39,18 +37,17 @@ app.use(session({
   resave: true,
   saveUninitialized: true,
   name: 'sessionId',
-}))
+}));
 
 // Express Messages Middleware
-app.use(require('connect-flash')())
+app.use(require('connect-flash')());
 app.use(function (req, res, next) {
-  res.locals.messages = require('express-messages')(req, res)
-  next()
-})
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.set("view engine", "ejs");
 app.use(expressLayouts);
@@ -60,40 +57,50 @@ app.use(cookieParser());
 
 app.use(utilities.checkJWTToken);
 
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
 /* ***********************
  * Routes
  *************************/
 app.use(static);
 
 // Index Route
-app.get("/", utilities.handleErrors(baseController.buildHome))
+app.get("/", utilities.handleErrors(baseController.buildHome));
 
 // Inventory routes
 app.use("/inv", inventoryRoute);
-// account routes
+
+// Account routes
 app.use("/account", accountRoutes);
-// classification routes
+
+// Classification routes
 app.use("/classification", classificationRoute);
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
-  next({ status: 404, message: 'Sorry, we appear to have lost that page.' })
-})
+  next({ status: 404, message: 'Sorry, we appear to have lost that page.' });
+});
 
 /* ***********************
 * Express Error Handler
 * Place after all other middleware
 *************************/
 app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav()
-  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  if (err.status == 404) { message = err.message } else { message = 'Oh no! There was a crash. Maybe try a different route?' }
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  if (err.status == 404) { 
+    message = err.message; 
+  } else { 
+    message = 'Oh no! There was a crash. Maybe try a different route?'; 
+  }
   res.render("errors/error", {
     title: err.status || 'Server Error',
     message,
     nav
-  })
-})
+  });
+});
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
